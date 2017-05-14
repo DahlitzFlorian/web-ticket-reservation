a28aed213b8256bbafe4de12fa8d908569976be1
florian dahlitz <f2dahlitz@freenet.de>
2017-05-14 22:15:13 +0200

    changed default controller

---
+++ app/controllers/Reservation.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *	Name: Reservation
+ *	Type: Controller
+ *	Description: Representing the reservation form of this system
+ *  			 accessable by everyone
+ *	
+ *	@author Florian Dahlitz
+ */
+class Reservation extends CI_Controller 
+{
+
+	public function index()
+	{
+		$this->load->view('reservation');
+	}
+}
---
+++ app/views/reservation.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $this->config->item('title'); ?></title>
+</head>
+<body>
+	<h1>Anmeldung Schlossführung</h1>
+</body>
+</html>
