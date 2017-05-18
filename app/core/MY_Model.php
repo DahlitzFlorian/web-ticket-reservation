32f86e98854be36b076d324f8d02258181c4d6c4
florian dahlitz <f2dahlitz@freenet.de>
2017-05-18 19:53:03 +0200

    added MY_Model - all models inherit from it

---
+++ app/core/MY_Model.php
@@ -0,0 +1,94 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Name: MY_Model
+ * Type: Core
+ * 
+ * @author Florian Dahlitz
+ */
+class MY_Model extends CI_Model
+{
+    /**
+     * default table
+     */
+    protected $table;
+    
+    /**
+     * validation rules
+     */
+    protected $validation_rules;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * get data
+     */
+    public function get_by($key, $value)
+    {
+        return $this->db->get_where($this->table, array($key => $value))->row();
+    }
+    
+    public function getAll($order_type = FALSE) // $order_type if existing as array[key, type]
+    {
+        if($order_type)
+            $this->db->order_by($order_type[0], $order_type[1]);
+        
+        return $this->db->get($this->table)->result();
+    }
+    
+    /**
+     * insert data
+     */    
+    public function add($data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
+    
+    public function add_ext($table, $data)
+    {
+        return $this->db->insert($table, $data);
+    }
+    
+    /**
+     * updating data
+     */
+    public function update($id, $data)
+    {
+        $this->db->where('id', $id);
+        return $this->db->update($this->table, $data);
+    }
+    
+    /**
+     * deleting data
+     */
+    public function delete($id)
+    {
+        return $this->db->delete($this->table, ['id' => $id]);
+    }
+    
+    /**
+     * counting data
+     */
+    public function count($obj)
+    {
+        return count((array)$obj);
+    }
+    
+    public function count_by($key, $value)
+    {
+        $this->db->get_where($this->table, [$key => $value])->result();
+        return $this->db->count_all_results($this->table);
+    }
+    
+    /**
+     * form validation
+     */
+    public function set_validation_rules()
+    {
+        $this->form_validation->set_rules($this->validation_rules);
+    }
+} 
